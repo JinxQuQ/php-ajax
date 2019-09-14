a80e42a64ca6222e30901ec525036dd291d42f3a
jinxwanan <821669288@qq.com>
2019-09-14 10:01:10 +0800

    英雄商城

---
+++ 2019-09/07.lol_list_detail/lol_detail.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+    <style>
+        div {
+            width: 300px;
+            height: 500px;
+            margin: auto;
+        }
+
+        img {
+            width: 200px;
+            height: 200px;
+            margin: 10px 50px;
+        }
+
+        h2 {
+            margin-bottom: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    //设置中文编码格式
+    header('content-type:text/html;charset=utf-8');
+
+    include 'data_lol_detail.php';
+    //获取键值对中的键
+    $heroName = $_GET['lolHero'];
+    //查询数据
+    //根据键获取值并赋予$hero
+    $hero = $heroArr[$heroName];
+    //print_r($hero)
+    //生成页面返回给用户
+    echo '<div><h2>' . $hero['champion_title'] . '--<span>' . $hero['champion_name'] . '</span></h2>
+    <br/>
+    <img src="' . $hero['champion_icon'] . '" alt=""><br/>
+    <h4>' . $hero['champion_info'] . '</h4>
+    <h4>' . $hero['champion_tags'] . '</h4>
+    <a href="./index.php">返回首页</a>
+    </div>'
+    ?>
+    
+</body>
+
+</html>
